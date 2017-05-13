0b596f26209043f64e52140e1df39dd54afa6a42
Erucolindo <erucolindo96@gmail.com>
2017-05-13 10:59:05 +0200

    Pozwala dodawać o jeden element za dużo
    Ponadto stwierdzono groźną anomalię - pewien konsument skonsumował dwa
    razy tą samą liczbę. Nie usunął jej co prawda z bufora(co zrobił dopiero
    drugi), ale odczytał dwa razy, a nie powinien.

--- Monitory/MonitorBuffer.hpp
+++ Monitory/MonitorBuffer.hpp
@@ -15,14 +15,15 @@ template<class T>
 class MonitorBuffer: public Monitor
 {
 public:
-    MonitorBuffer(const int MAX_ITEMS):MAX_ITEMS_(MAX_ITEMS)
+    MonitorBuffer(const unsigned int MAX_ITEMS):MAX_ITEMS_(MAX_ITEMS)
     {}
 
     void pushOneElement(const T &element)
     {
         enter();
-        if(buffer_.size() == MAX_ITEMS_)
+        while(buffer_.size() >= MAX_ITEMS_)
         {
+            //wait(canPush);
             wait(canPushOne);//czekaj dopoki konsumenci nie zwolnia elementu
         }
         buffer_.push_back(element);
@@ -30,8 +31,7 @@ public:
         if(buffer_.size() >= MIN_ELEM_)
         {
             signal(canPop);
-            signal(canPopFirst);
-            signal(canPopSecond);
+
         }
 
         leave();
@@ -41,9 +41,10 @@ public:
     {
         enter();
 
-        if(buffer_.size() >= MAX_ITEMS_ - 1)//jesli w buforze jest albo tylko jedno miejsce, albo nie ma go wcale
+        while(buffer_.size() >= MAX_ITEMS_ - 1)//jesli w buforze jest albo tylko jedno miejsce, albo nie ma go wcale
         {
-            wait(canPushTwo);
+           // wait(canPush);
+            wait(canPushTwo);//cos jest nie tak z producentami, pozwalaja dodawac za duzo elementow
         }
         buffer_.push_back(elem_1);
         buffer_.push_back(elem_2);
@@ -51,8 +52,6 @@ public:
         if(buffer_.size() >= MIN_ELEM_)
         {
             signal(canPop);
-            signal(canPopFirst);
-            signal(canPopSecond);
 
         }
 
@@ -64,65 +63,117 @@ public:
         T returned_elem;
         enter();
 
-        if(buffer_.size() < MIN_ELEM_)//jesli jest mniejsza niz MIN - to gdy element zostanie dodany, to albo będzie wiecej niz MIN_ELEm
-        {                           //albo ==MIN_ELEM, ale bedziemy pierwszymi ktorzy wyjmuja
-            wait(canPop);
-            if(wasOtherConsumentPop)//mozemy usunac element
-            {
-                returned_elem = PopAndDelete();
-                wasOtherConsumentPop = false;
-            }
-            else//nie mozemy trwale usunac, ale mozemy pobrac
-            {
-                returned_elem = buffer_.front();
-                wasOtherConsumentPop = true;
-            }
-
-        }
-        else if(buffer_.size() == MIN_ELEM_)//jest minimalna liczba elementow - jeden moze usuwac
+        if(buffer_.size() < MIN_ELEM_  )//jesli jest zbyt malo elementow w buforze
         {
-            if(wasOtherConsumentPop)//musimy poczekac az ktorys producent wstawi i  usunac element
+            wait(canPop);//czekamy
+            //teraz mamy 3 lub 4 elementy
+            if(buffer_.size() == MIN_ELEM_)
             {
-                wait(canPop);
-                returned_elem = PopAndDelete();
-                wasOtherConsumentPop = false;
+                returned_elem = situationMIN_ELEM_forFirst();
             }
-            else//nie mozemy trwale usunac, ale mozemy pobrac
+            else
             {
-                returned_elem = buffer_.front();
-                wasOtherConsumentPop = true;
+                returned_elem = situationNormalForFirst();
             }
 
         }
-        else//jest wiecej niz MIN_ELEM
+        else if(buffer_.size() == MIN_ELEM_)//wtedy nie mozna usunac elementu
         {
-            if(wasOtherConsumentPop)//mozemy usunac element
-            {
-                returned_elem = PopAndDelete();
-                wasOtherConsumentPop = false;
-            }
-            else//nie mozemy trwale usunac, ale mozemy pobrac
-            {
-                returned_elem = buffer_.front();
-                wasOtherConsumentPop = true;
-            }
+            returned_elem = situationMIN_ELEM_forFirst();
+        }
+        else //jest wiecej niz MIN_ELEM - mozna swobodnie usuwac
+        {
+            returned_elem = situationNormalForFirst();
         }
 
-        if(buffer_.size() < MIN_ELEM_ - 1)//jesli sa dwa wolne miejsca
+        if(!wasFirstConsumentPop && !wasSecondConsumentPop)//jesli obydwie flagi konsumentów == false, to element został usunięty, a nie tylko przeczytany
+        {//wiec mozemy zasygnalizowac mozliwosc dodawania
+
+            if(buffer_.size() < MAX_ITEMS_ - 1)//jesli sa chociaz dwa wolne miejsca
+                signal(canPushTwo);
+
+            if(buffer_.size() < MAX_ITEMS_)//jesli jest chociaz jedno wolne miejsce
+                signal(canPushOne);
+        }
+
+
+/*
+        if(buffer_.size() < MAX_ITEMS_)
+            signal(canPush);
+
+        if(buffer_.size() < MAX_ITEMS_ - 1)//jesli sa chociaz dwa wolne miejsca
             signal(canPushTwo);
 
-        signal(canPushOne);
-
+        if(buffer_.size() < MAX_ITEMS_)//jesli jest chociaz jedno wolne miejsce
+            signal(canPushOne);
+*/
         leave();
         return returned_elem;
+
     }
 
     T popConsumentSecond()
     {
-        return popConsumentFirst();
+        T returned_elem;
+        enter();
+
+        if(buffer_.size() < MIN_ELEM_  )//jesli jest zbyt malo elementow w buforze
+        {
+            wait(canPop);//czekamy
+            //teraz mamy 3 lub 4 elementy
+            if(buffer_.size() == MIN_ELEM_)
+            {
+                returned_elem = situationMIN_ELEM_forSecond();
+            }
+            else
+            {
+                returned_elem = situationNormalForSecond();
+            }
+
+        }
+        else if(buffer_.size() == MIN_ELEM_)//wtedy nie mozna usunac elementu
+        {
+            returned_elem = situationMIN_ELEM_forSecond();
+        }
+        else //jest wiecej niz MIN_ELEM - mozna swobodnie usuwac
+        {
+            returned_elem = situationNormalForSecond();
+        }
+
+
+        if(!wasFirstConsumentPop && !wasSecondConsumentPop)//jesli obydwie flagi konsumentów == false, to element został usunięty, a nie tylko przeczytany
+        {//wiec mozemy zasygnalizowac mozliwosc dodawania
+
+            if(buffer_.size() < MAX_ITEMS_ - 1)//jesli sa chociaz dwa wolne miejsca
+                signal(canPushTwo);
+
+            if(buffer_.size() < MAX_ITEMS_)//jesli jest chociaz jedno wolne miejsce
+                signal(canPushOne);
+        }
+/*        if(buffer_.size() < MAX_ITEMS_)
+            signal(canPush);
+
+        if(buffer_.size() < MAX_ITEMS_ - 1)//jesli sa chociaz dwa wolne miejsca
+            signal(canPushTwo);
+
+        if(buffer_.size() < MAX_ITEMS_)//jesli jest chociaz jedno wolne miejsce
+            signal(canPushOne);
+*/
+        leave();
+        return returned_elem;
+
     }
 
 
+    unsigned int size() const
+    {
+        int ret_val;
+
+        enter();
+        ret_val = buffer_.size();
+        leave();
+        return ret_val;
+    }
 
 
 private:
@@ -138,19 +189,86 @@ private:
         auto first_elem  = buffer_.begin();
         T temp = *first_elem;
         buffer_.erase(first_elem);
-        return T;
+        return temp;
     }
 
-    Condition canPushOne, canPushTwo, canPush;
-    Condition canPopFirst, canPopSecond, canPop;
+    T situationNormalForFirst()
+    {
+        T ret;
+        if(wasSecondConsumentPop)
+        {
+            ret = PopAndDelete();
+            wasSecondConsumentPop = false;
+        }
+        else
+        {
+            ret = buffer_.front();
+            wasFirstConsumentPop = true;
+        }
+        return ret;
+    }
+
+    T situationNormalForSecond()
+    {
+        T ret;
+        if(wasFirstConsumentPop)
+        {
+            ret = PopAndDelete();
+            wasFirstConsumentPop = false;
+        }
+        else
+        {
+            ret = buffer_.front();
+            wasSecondConsumentPop = true;
+        }
+        return ret;
+    }
+
+
+    T situationMIN_ELEM_forFirst()
+    {
+        T ret;
+        if(wasSecondConsumentPop)
+        {
+            wait(canPop);//zaczekaj az ktos doda element, potem usun
+            ret = PopAndDelete();
+            wasSecondConsumentPop = false;
+        }
+        else//drugi konsument nie odczytal elementu - czytamy bez usuwania
+        {
+            ret = buffer_.front();
+            wasFirstConsumentPop = true;
+        }
+        return ret;
+    }
+
+    T situationMIN_ELEM_forSecond()
+    {
+        T ret;
+        if(wasFirstConsumentPop)//nie mozemy trwale usunac elementu, bo w buforze bylo by ich za malo
+        {
+            wait(canPop);//zaczekaj az ktos doda element, potem usun
+            ret = PopAndDelete();
+            wasFirstConsumentPop = false;
+        }
+        else//drugi konsument nie odczytal elementu - czytamy bez usuwania
+        {
+            ret = buffer_.front();
+            wasSecondConsumentPop = true;
+        }
+        return ret;
+    }
+
+    mutable Condition canPushOne, canPushTwo, canPush;
+    mutable Condition canPop;//, canDeleteFirstElem;
 
     std::vector<T> buffer_;
-    T elementToPop;
+    //T elementToPop;
 
-    bool wasOtherConsumentPop;//mowi czy drugi konsument usunał ten element
+    mutable bool wasFirstConsumentPop, wasSecondConsumentPop;//mowi czy drugi konsument usunał ten element
 
-    const int MAX_ITEMS_;
-    const int MIN_ELEM_ = 3;//minimalna ilość elementów - gdy w buforze jest więcej elementów to można je wyciągać z bufora
+    const unsigned int MAX_ITEMS_;
+    const unsigned int MIN_ELEM_ = 3;//minimalna ilość elementów - gdy w buforze jest więcej elementów to można je wyciągać z bufora
 
 
 
--- Monitory/main.cpp
+++ Monitory/main.cpp
@@ -1,9 +1,76 @@
 #include <QCoreApplication>
+#include<thread>
+#include"MonitorBuffer.hpp"
+#include<iostream>
+#include<unistd.h>
+const int BUF_SIZE = 10;
+
+MonitorBuffer<int> my_buffer(BUF_SIZE);
+
+
+void producerOneCode()
+{
+    std::cout<<"Producent 1 gotowy!"<<" "<<std::endl;
+    for(int i = 0; i<10;++i)
+    {
+        my_buffer.pushOneElement(i);
+        std::cerr<<"P1: Dodano element "<<i<<" "<<std::endl;
+        sleep(1);
+    }
+}
+
+void producerDoubleCode()
+{
+    std::cout<<"Producent 2 gotowy!"<<std::endl;
+    for(int i = 10; i<20;i+=2 )
+    {
+        my_buffer.pushTwoElements(i, i+1);
+        std::cerr<<"P2: Dodano elementy "<<i<<" oraz "<<i+1<<" "<<std::endl;
+        sleep(3);
+    }
+}
+
+void consumentFirstCode()
+{
+    int ret;
+    std::cout<<"Konsument 1 gotowy!"<<" "<<std::endl;
+    for(int i = 0; i<10;++i)
+    {
+        ret = my_buffer.popConsumentFirst();
+        std::cerr<<"K1: Zdjeto element "<<ret<<" "<<std::endl;
+        sleep(5);
+    }
+}
+
+void consumentSecondCode()
+{
+    int ret;
+    std::cout<<"Konsument 2 gotowy!"<<" "<<std::endl;
+    for(int i = 0; i<10;++i)
+    {
+        ret = my_buffer.popConsumentSecond();
+        std::cerr<<"K2: Zdjeto element "<<ret<<" "<<std::endl;
+        sleep(4);
+    }
+}
+
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
 
-    return a.exec();
+
+    std::thread producer_one(producerOneCode), producer_double(producerDoubleCode);
+    sleep(1);
+    std::thread consument_first(consumentFirstCode), consument_second(consumentSecondCode);
+
+
+    producer_one.join();
+    producer_double.join();
+    consument_first.join();
+    consument_second.join();
+
+    std::cout<<"Wszystkie watki wykonaly swoja prace"<<std::endl;
+
+    return 0;
 }
 
